2b90517f4b7cef591b83cbfc42ccff6d6f40cd25
Syamsulhadi9 <73639766+Syamsulhadi9@users.noreply.github.com>
2021-09-30 15:51:13 +0800

    Add files via upload

---
+++ Hobi.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOBI SAYA</title>
+</head>
+<body bgcolor="Green">
+<p><?= $hobi ?></p>
+<center> 
+<a href="http://localhost/ci4/public/home/index">home</a>
+<a href="http://localhost/ci4/public/home/riwayat_pendidikan">riwayat pendidikan</a><h5> fork by Syamsul Hadi </h5><br></center>
+
+
+
+</body>
+</html>
--- Home.php
+++ Home.php
@@ -1,1 +1,72 @@
-// Controller Home.php
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    { 
+    echo "<center><p><B>BERIKUT ADALAH DATA DIRI SAYA  </B></p>";
+        $index =[
+        	"Nama"=>"SYAMSUL HADI",
+            "Nim"=>" TI16190020 ",
+        	"Alamat"=>" BUJAK "];
+            echo "Nama :".$index["Nama"]."<br>";
+            echo "Nim :".$index["Nim"]."<br>";
+            echo "Alamat :".$index["Alamat"]."<br></center>";
+        return view('Home_view', $index);
+    }
+
+    public function Hobi()
+    {
+    	echo "<center><p><B>BERIKUT ADALAH HOBI SAYA </B></p>";
+        $hobi = [" MAKAN "," MINUM "," MANCING "," NONGKRONG "," NGEHALU "];
+        	
+            echo $hobi[0]."<br>"."<br>";
+            echo $hobi[1]."<br>"."<br>";
+            echo $hobi[2]."<br>"."<br>";
+            echo $hobi[3]."<br>"."<br>";
+            echo $hobi[4]."<br>"."<br></center>";
+
+        return view('Hobi', $hobi);
+    }
+    public function riwayat_pendidikan()
+    {
+    	echo "<center><p><B>BERIKUT ADALAH DAFTAR RIWAYAT PENDIDIKAN SAYA</B></p></center>";
+    	
+    	$pendidikan= ["Nama Sekolah"=> "SDN GUNUNG AMUK",
+    					"Tahun lulus"=>  2004,];
+    				echo "<center> <p>1. SD <br></p>nama sekolah : ".$pendidikan["nama sekolah"]."<br>"; 
+    				echo "Tahun lulus : ".$pendidikan["Tahun lulus"]."<br>"."<br>";
+        //SMP
+      
+    	$pendidikan=["Nama Sekolah"=>"SMPN 4 BATUKLIANG",
+    				"Tahun lulus"=>2007,];
+
+    		echo "<center><p>2. SMP <br></p>Nama Sekolah : ".$pendidikan["nama sekolah"]."<br>"; 
+    		echo "Tahun lulus : ".$pendidikan["Tahun lulus"]."<br>"."<br>";
+
+
+    	//SMA
+    		$pendidikan=["Nama Sekolah"=>"SMAI USWATUN HASANAH CEMPAKA PUTIH",
+    				"Jurusan"=>"BAHASA",
+    				"Tahun Lulus"=>2011,];
+    		echo "<p>3. SMA <br></p>Nama Sekolah : ".$pendidikan["nama sekolah"]."<br>"; 
+    		echo "Tahun lulus : ".$pendidikan["Tahun Lulus"]."<br>";
+    		echo "Jurusan : ".$pendidikan["Jurusan"]."<br>"."<br>";
+
+    	//KAMPUS
+    		$pendidikan=["Nama Kampus"=>"STMIK LOMBOK",
+    				"Prodi"=>"TEHNIK INFORMATIKA",
+    				"Semester"=> 5,
+    				];
+
+    		echo "<p>4. KULIAH <br></p>Nama Kampus : ".$pendidikan["Nama Kampus"]."<br>"; 
+    		echo "Semester : ".$pendidikan["Semester"]."<br>";
+    		echo "Prodi : ".$pendidikan["Prodi"]."<br>"."<br>";
+ 
+ 
+ 
+        return view('riwayat_pendidikan',$pendidikan);
+    }
+}
